55efafe4c7015d185b6281ff857a21e7d6eaeadf
Hooopo Wang <hoooopo@gmail.com>
2019-03-22 15:36:50 +0800

    add args for seed

--- seed.sql
+++ seed.sql
@@ -1,8 +1,9 @@
-insert into jobs (at, enqueued_at, created_at, jobtype) 
+insert into jobs (at, enqueued_at, created_at, jobtype, args) 
 select now() - (interval '12 hour') + random() * interval '1 day' as at, 
        now() as enqueued_at, 
        now() as created_at, 
-       'HelloJob' 
+       'HelloJob' as jobtype,
+       '[1, "a"]' as args
   from generate_series(1, 1000000);
 
   vacuum full jobs;
