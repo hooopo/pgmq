e0b0ffd400f01fdebee97a10e6ee762fae2da59b
Hooopo Wang <hoooopo@gmail.com>
2019-03-24 08:28:09 +0800

    ignore bundle

--- seed.sql
+++ seed.sql
@@ -2,6 +2,6 @@ insert into jobs (created_at, jobtype, args)
 select now() as created_at, 
        'HelloJob' as jobtype,
        '[1, "a"]' as args
-  from generate_series(1, 1000000);
+  from generate_series(1, 100000);
 
   vacuum full jobs;
